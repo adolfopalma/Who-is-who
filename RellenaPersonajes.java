d421fb8fd893e5bcb50cb99f1581bd56510dd5d1
sergiomartinez1984 <sounstudio@hotmail.com>
2021-01-22 21:02:39 +0100

    adolfo he cambiado algunos nombres

--- RellenaPersonajes.java
+++ RellenaPersonajes.java
@@ -7,8 +7,8 @@ public class RellenaPersonajes {
         List<Personajes> personajes = new ArrayList<Personajes>();
         personajes.add(new Personajes("Sergio", "Castaño", "Hombre", "Blanca", "Gafas", "Marrones", "Barba"));
         personajes.add(new Personajes("Adolfo", "Castaño", "Hombre", "Blanca", "Gafas", "Azules", "Barba"));
-        personajes.add(new Personajes("Sergio", "Castaño", "Hombre", "Blanca", "Gafas", "Verdes", "Barba"));
-        personajes.add(new Personajes("Sergio", "Castaño", "Hombre", "Blanca", "Gafas", "Marrones", "Barba"));
+        personajes.add(new Personajes("Ines", "Castaño", "Hombre", "Blanca", "Gafas", "Verdes", "Barba"));
+        personajes.add(new Personajes("Pablo", "Castaño", "Hombre", "Blanca", "Gafas", "Marrones", "Barba"));
         personajes.add(new Personajes("Sergio", "Castaño", "Hombre", "Blanca", "Gafas", "Marrones", "Barba"));
         personajes.add(new Personajes("Sergio", "Castaño", "Hombre", "Blanca", "Gafas", "Marrones", "Barba"));
         personajes.add(new Personajes("Sergio", "Castaño", "Hombre", "Blanca", "Gafas", "Verdes", "Barba"));
